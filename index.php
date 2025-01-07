8bebf95a59572f76c86292df3ce300381781a894
ipb23.a.eglitis <ipb23.a.eglitis@pjvt.local>
2025-01-07 14:13:22 +0200

    sql injection

--- index.php
+++ index.php
@@ -4,21 +4,27 @@ require "functions.php";
 require "Database.php";
 
 $config = require("config.php");
-
-
-
 $db = new Database($config["database"]);
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
-
-//var_dump(isset($_GET["search_query"]));
+$select = "SELECT * FROM posts";
+$params = [];
 
 if(isset($_GET["search_query"]) && $_GET["search_query"] !=""){
 
-    echo "Atgriezt ierakstus";
+$search_query = "%" . $_GET["search_query"] . "%"; 
+
+$select .=" WHERE content LIKE: nosaukums"; 
+
+$params = ["nosaukums"=>$search_query] ;
+
+
+    //dd("SELECT*FROM posts WHERE content LIKE '% " . $_GET["search_query"]'%");
 
     // TODO: Iegt filtretos ierakstus
-$posts = $db->query("SELECT*FROM posts WHERE content LIKE '%". $_GET["search_query"] . "%';")->fetchAll();
+
 }
+$posts = $db->query($select, $params)->fetchAll();
+
+
 
 echo "<h1> Blogs</h1>";
 
@@ -33,6 +39,10 @@ echo "<buttton>Meklet</button>";
 
 
 echo "</form>";
+
+if(count($posts) == 0  ){
+    echo "Not found";
+}
 
 
 echo "<ul>";
