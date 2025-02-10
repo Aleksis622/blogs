fd79d655e485e8e0014258003d906bb50e8910f5
ipb23.a.eglitis <ipb23.a.eglitis@pjvt.local>
2025-02-10 09:48:31 +0200

    nothing done

--- controllers/posts/create.php
+++ controllers/posts/create.php
@@ -1,22 +1,28 @@
-<?php 
+<?php
 
+$sql = "INSERT INTO posts VALUES(content)" ;
 $select = "SELECT * FROM posts";
-$params = [];
+$params = ["content"=> $_POST["content"]];
 
-if(isset($_GET["search_query"]) && $_GET["search_query"] !=""){
+
+if(isset($_POST["content"]) && $_POST["content"] !=""){
 
 $search_query = "%" . $_GET["search_query"] . "%"; 
 
 $select .=" WHERE content LIKE: nosaukums"; 
 
-$params = ["nosaukums"=>$search_query] ;
+//$params = ["nosaukums"=>$search_query] ;
 }
 
 $posts = $db->query($select, $params)->fetchAll();
 
+if($_SERVER["REQUEST_METHOD"] == $_POST){
+    $sql;
+}
+
 dd($_SERVER["REQUEST_METHOD"]);
 
-
+header("Location: /"); exit();
 
 require "views/posts/create.view.php";
 ?>
