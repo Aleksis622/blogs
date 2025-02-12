f0b989be7a454ff3d0aba014734273853b471159
ipb23.a.eglitis <ipb23.a.eglitis@pjvt.local>
2025-02-12 13:49:47 +0200

    nothing

--- controllers/posts/create.php
+++ controllers/posts/create.php
@@ -1,28 +1,29 @@
 <?php
 
-$sql = "INSERT INTO posts VALUES(content)" ;
-$select = "SELECT * FROM posts";
-$params = ["content"=> $_POST["content"]];
+$sql = "INSERT INTO posts (content) VALUES(content)" ;
 
 
 if(isset($_POST["content"]) && $_POST["content"] !=""){
 
-$search_query = "%" . $_GET["search_query"] . "%"; 
+$search_query = "%" . $_POST["content"] . "%"; 
 
-$select .=" WHERE content LIKE: nosaukums"; 
-
-//$params = ["nosaukums"=>$search_query] ;
+$params = ["nosaukums"=>$search_query] ;
 }
+dd($_SERVER["REQUEST_METHOD"]);
 
 $posts = $db->query($select, $params)->fetchAll();
 
-if($_SERVER["REQUEST_METHOD"] == $_POST){
-    $sql;
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["content"]) && $_POST["content"] != "") {
+    // Execute the insert query to add content to the posts table
+    $prepare = $db->prepare($sql);
+    $execute->execute(["content" => $_POST["content"]]);
+    
+    // Redirect after insertion
+    header("Location: /");
+    exit();
 }
 
-dd($_SERVER["REQUEST_METHOD"]);
 
-header("Location: /"); exit();
 
 require "views/posts/create.view.php";
 ?>
