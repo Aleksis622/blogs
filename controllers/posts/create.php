c64de84b34b07ce82aeec652165a6420aad2b18d
ipb23.a.eglitis <ipb23.a.eglitis@pjvt.local>
2025-02-14 12:23:49 +0200

    gf

--- controllers/posts/create.php
+++ controllers/posts/create.php
@@ -1,6 +1,6 @@
 <?php
 
-$sql = "INSERT INTO posts (content) VALUES(content)" ;
+$sql = "INSERT INTO posts (content) VALUES(:content)" ;
 
 
 if(isset($_POST["content"]) && $_POST["content"] !=""){
@@ -11,11 +11,11 @@ $params = ["nosaukums"=>$search_query] ;
 }
 dd($_SERVER["REQUEST_METHOD"]);
 
-$posts = $db->query($select, $params)->fetchAll();
+$posts = $db->query( $params)->fetchAll();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["content"]) && $_POST["content"] != "") {
     // Execute the insert query to add content to the posts table
-    $prepare = $db->prepare($sql);
+    $prepare->execute(["content" => $_POST["content"]]);
     $execute->execute(["content" => $_POST["content"]]);
     
     // Redirect after insertion
