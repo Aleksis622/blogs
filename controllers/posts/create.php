7e4f71a73162e1f4355afe6787285aec163e38f7
ipb23.a.eglitis <ipb23.a.eglitis@pjvt.local>
2025-02-17 11:27:49 +0200

    something

--- controllers/posts/create.php
+++ controllers/posts/create.php
@@ -16,13 +16,14 @@ header("Location: /");
 exit();
 
 }
-if( !isset($_POST["content"]) == true ||  strlen($_POST["content"])== 0  || $_POST["content"] == 50)  {
+if( !isset($_POST["content"]) ||  strlen($_POST["content"])== 0  || strlen( $_POST["content"]) > 50)  {
   
    $errors["content"] ="Saturam jābūt ievadītam, bet ne garākam par 50 rakstzīmēm";
 }
 
 if(empty($errors)){
     require_once "views/posts/create.view.php";
+   
 }
 
 
--- views/posts/create.view.php
+++ views/posts/create.view.php
@@ -16,5 +16,4 @@
 
 
 
-
 <?php require "views/components/footer.php";?>
